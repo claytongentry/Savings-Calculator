4341e8e97b03e550f3bc643549cdbd62e822cbfa
cg3ntry <cg3ntry@gmail.com>
2015-04-19 10:18:30 -0500

    Architecture refreshed

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,9 @@
         <meta charset = "utf-8">
         <link type = "text/css" rel = "stylesheet" href = "jquery-ui.min.css">
         <link type = "text/css" rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-        <link type = "text/css" rel="stylesheet" href="../styles.css">
+        <link type = "text/css" rel="stylesheet" href="styles.css">
         <script type = "text/javascript" src = "https://www.google.com/jsapi"></script>
-        <script type = "text/javascript" src = "../gaugeChart.js"></script>
+        <script type = "text/javascript" src = "gaugeChart.js"></script>
         <title>SHOP Savings Calculator</title>
     </head>
     <body id = "calculator">

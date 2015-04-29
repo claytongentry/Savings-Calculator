ca86bafe68476b49924dcd469d64d37ed1695b3b
cg3ntry <cg3ntry@gmail.com>
2015-04-29 14:28:15 -0500

    Meta scale added to header

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html lang = "en" id = "SHOP Savings Calcator">
     <head>
         <meta charset = "utf-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
         <link type = "text/css" rel = "stylesheet" href = "jquery-ui.min.css">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
         <link type = "text/css" rel="stylesheet" href="styles.css">
